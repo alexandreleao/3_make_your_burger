86399bcd3dceed74a05f8de5c6b59de7f608892b
Alexandre <erdleosalgueiro@gmail.com>
2022-03-13 18:25:30 -0300

    Criação do Layout da página home e pedidos

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <p>Footer</p>
-    </div>
+    <footer id="footer">
+        <p>Make Your Burger &copy; 2022</p>
+    </footer>
 </template>
 
 <script>
@@ -10,3 +10,17 @@
    }
 
 </script>
+
+<style scoped>
+     #footer{
+     height: 200px;
+     background-color: #222;
+     border-top: 4px solid #111;
+     color: #FCBA03;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     width: 100%;
+     }
+
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
    <div id="nav">
-   <router-link to="/">
-     <img src="" id="logo">
+   <router-link to="/" id="logo-url">
+     <img :src="logo" :alt="alt" id="logo">
    </router-link>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link> 
     <router-link to="/pedido">Pedidos</router-link>
   </div>
 </template>
@@ -11,6 +11,7 @@
 <script>
 export default {
     name: "Navbar",
+    props:["logo", "alt"],
     data(){
         return{
 
@@ -20,5 +21,33 @@ export default {
 </script>
 
 <style scoped>
+     #nav{
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+   }
 
+   #nav #logo-url{
+        margin: auto;
+        margin-left: 0;
+   }
+
+   #logo{
+     width: 40px;
+     height: 40px;   
+   }
+
+   #nav a{
+    color: #FCBA03;
+    text-decoration: none;
+    margin: 12px;
+    transition: .5s;
+   }
+
+   #nav a:hover{
+    color: #FFF;
+   }
 </style>
