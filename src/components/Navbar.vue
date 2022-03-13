2b4d06a34fd038cd60990ccdcdbc1427bbda3b92
Alexandre <erdleosalgueiro@gmail.com>
2022-03-13 17:47:14 -0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+   <div id="nav">
+   <router-link to="/">
+     <img src="" id="logo">
+   </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedido">Pedidos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
