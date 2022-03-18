573c0a35fb00b76ae7a62a49676c0fe5cc187b22
Alexandre <erdleosalgueiro@gmail.com>
2022-03-18 11:03:14 -0300

    Inserção de dados no banco, imagem de pedido com número, limpar mensagens e arquivo de Message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
 <Banner />
   <div class="main-container">
-       <h1>Monte o seu burger:</h1>
-       <p>formulário</p>
+      <h1>Monte o seu burger:</h1>
+<BurgerForm />     
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue"
+import BurgerForm from "../components/BurgerForm.vue"
 
 export default {
   name:"Home",
   components:{
     Banner,
+    BurgerForm,
+    
 }
 
 }
